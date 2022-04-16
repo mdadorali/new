1cb1096d53031d1f0af82d528ed43399ec34192e
mdadorali <101986539+mdadorali@users.noreply.github.com>
2022-04-16 17:54:14 +0600

    assingment 5

--- index.php
+++ index.php
@@ -1,49 +1,55 @@
 <?php
-    //   Question 02:Operator:
-    
-  
-//   Question 02:Operator: Increment (++) ,For Loop, Assignment(=), Comparison(!==,<=)
 
-for ($n=0; $n <= 100; $n++) {
-    if ($n % 2 !== 0 ) 
-    echo"$n" . "<br>";
+// Question 01: factors, prime number.
+
+$given_array =[12,34,2,6,78];
+foreach ($given_array as $key => $number) {
+    $is_prime = true;
+    for( $i =2; $i <= $number-1; $i++) {
+        if($number % $i ==0) {
+            $is_prime =false;
+        }
+    }
+
+    if ( $is_prime) {
+        echo "$number is a prime number <br>";
+    } else {
+        echo "$number is not a prime number <br>";
+    }
+}
+
+// Question 02: daraz website and pick any 5 products
+
+$products =['realmi_9i ( ram&rom 4&64 price:5000)' ,'oppo_a16 ( ram&rom 6&128 price:50000)','vivo_y21 ( ram&rom 4&64 price:8000)','poco_m3 ( ram&rom 4&64 price:9000)','motorola_edge ( ram&rom 4&64 price:5000)'
+];
+
+foreach ( $products as $name) {
+    echo $name;
+    echo "<br>";
+}
+
+// Question 03: maximum valu foreach loop.
+
+$given_array = array(0,10,80,67,60,89,91,56,45,30,95,83,99);
+$max_value = 0;
+
+foreach ($given_array as $key => $number) {
+    if ( $number > $max_value) {
+        $max_value = $number;
+    }
+}
+
+echo "This is our max number $max_value <br>";
+
+// Question 04: this array(0,10,80,67,60,89,91,56,45,30,95,83,99) and print 0 index
+
+$given_array = array(0,10,80,67,60,89,91,56,45,30,95,83,99);
+foreach ($given_array as $key => $number) {
+    if ( $key % 2 ==0) {
+        echo "$number <br>";
+    }
 }
 
 
-    // Question 03:Operator:Decrement (--),Comparison(>=) calculate the factorial
 
-$num = 4;
-$factorial = 1;
-for($n=$num; $n >=1; $n--)
-{
-    $factorial = $factorial *$n;
-}
-echo"Factorial of $num is $factorial" . "<br>";
-
-$num = 7;
-$factorial = 1;
-for($n=$num; $n >=1; $n--)
-{
-    $factorial = $factorial *$n;
-}
-echo"Factorial of $num is $factorial" . "<br>";
-
-
-// Quetion04;Operator:Decrement (--), print
-
-$n = 100;
-
-while($n >= 1) {
-  echo " $n <br>";
-  $n--;
-}
-    // Quetion:05 Operator:(Foreach)
-
-$products = ['apple', 'orange', 'banana', 'mango'];
-
-
-foreach ($products as $key => $name){
-echo $key.'-'. $name;
-echo"<br>";
-}
 ?>
